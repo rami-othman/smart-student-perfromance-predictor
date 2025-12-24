41f1797fce8cb9d8e2871af9d366cf5c4dca98d5
MSI\Rami <ramiotham03@gmail.com>
2025-12-24 16:28:34 +0300

    first try

---
+++ src/data.py
@@ -0,0 +1,21 @@
+
+# Libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# from tabulate import tabulate
+from pprint import pprint
+
+math_score = pd.read_csv('raw student data/student-mat.csv')
+portuguese_score = pd.read_csv('raw student data/student-por.csv')
+
+# print("\n--- Column Names ---")
+# print(*math_score.columns, sep=';')
+
+# col_data = [[i, col] for i, col in enumerate(math_score.columns)]
+# print("\nDataset Column Overview:")
+# print(tabulate(col_data, headers=["Index", "Column Name"], tablefmt="grid"))
+
+print("\n--- Pretty Printed Columns ---")
+print(math_score.dtypes)
